fe6be736b608beb98022b9819c342b590ad20f97
bribironke <br.ibironke@gmail.com>
2023-06-09 01:34:59 +0100

    removed supabase

--- functions/listtasks.ts
+++ functions/listtasks.ts
@@ -1,15 +1,21 @@
 
 import { Handler } from "@netlify/functions";
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 import "dotenv/config"
 
 export const handler: Handler = async () => {
 
-  const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_KEY as string
-  )
-  const { data } = await supabase.from('task').select('*')
+  // const supabase = createClient(
+  //   process.env.SUPABASE_URL as string,
+  //   process.env.SUPABASE_KEY as string
+  // )
+  // const { data } = await supabase.from('task').select('*')
+
+  const data = [
+    { name: `Gabriel ${process.env.SUPABASE_URL}`, id: 1 },
+    { name: `Lanre ${process.env.SUPABASE_KEY}`, id: 2 },
+    { name: 'Ibironke', id: 3 },
+  ]
 
   return {
     statusCode: 200,
