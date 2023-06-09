9d7cc3fe9843a17a7c7eb7db0f83f8034e3cdb6f
bribironke <br.ibironke@gmail.com>
2023-06-09 01:17:23 +0100

    removed supabase

--- functions/listtasks.ts
+++ functions/listtasks.ts
@@ -1,11 +1,16 @@
 
 import { Handler } from "@netlify/functions";
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
-)
+// export const supabase = createClient(
+//   process.env.SUPABASE_URL as string,
+//   process.env.SUPABASE_KEY as string
+// )
+
+// export const supabase = createClient(
+//   process.env.SUPABASE_URL as string,
+//   process.env.SUPABASE_KEY as string
+// )
 
 export const handler: Handler = async () => { 
   // const { data } = await supabase.from('task').select('*')
