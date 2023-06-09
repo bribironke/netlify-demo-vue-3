14287db91cb799cb6fac2ee4612486eecb9d4e27
bribironke <br.ibironke@gmail.com>
2023-06-09 01:23:07 +0100

    added supabase url & key

--- functions/listtasks.ts
+++ functions/listtasks.ts
@@ -2,10 +2,10 @@
 import { Handler } from "@netlify/functions";
 import { createClient } from '@supabase/supabase-js'
 
-// export const supabase = createClient(
-//   process.env.SUPABASE_URL as string,
-//   process.env.SUPABASE_KEY as string
-// )
+export const supabase = createClient(
+  "https://tzhcdjavlubkbccsqcdf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aGNkamF2bHVia2JjY3NxY2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4NjQwMTMsImV4cCI6MTk5OTQ0MDAxM30.1t_wJDceRTi6JohruwDq9snJV_TN0YfCxWlsTLab1c8"
+)
 
 // export const supabase = createClient(
 //   process.env.SUPABASE_URL as string,
